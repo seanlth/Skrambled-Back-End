b1a44f0acb4616ac8e1454721fb7d4bbadd68e0a
hargados <hargados@tcd.ie>
2015-03-14 15:25:20 +0000

    init

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  SkrambledBackend
+//
+//  Created by Seán Hargadon on 14/03/2015.
+//  Copyright (c) 2015 Seán Hargadon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+    
+    
+    return 0;
+}

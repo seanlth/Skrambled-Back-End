019501e7658eeb88dba3563885d6080ed1078b2a
hargados <hargados@tcd.ie>
2015-03-16 21:31:50 +0000

    Added DHKE

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@
 //
 
 #include <iostream>
-#include <sqlite3.h>
+#include <iomanip>
 #include <stdio.h>
+
+#include <sqlite3.h>
+
 #include <gmp.h>
 #include <gmpxx.h>
 
@@ -45,25 +48,87 @@ struct database {
         
         this->rc = sqlite3_step(this->res);
         
-        if (this->rc == SQLITE_ROW) {
-            printf("%s\n", sqlite3_column_text(this->res, 0));
-            printf("%s\n", sqlite3_column_text(this->res, 1));
-        }
+//        if (this->rc == SQLITE_ROW) {
+//            printf("%s\n", sqlite3_column_text(this->res, 0));
+//            printf("%s\n", sqlite3_column_text(this->res, 1));
+//        }
         sqlite3_finalize(this->res);
     }
     
+    void remove(std::string table, std::string name)
+    {
+        std::string command = "delete from " + table + " where name =\"" + name + "\";";
+        
+        rc = sqlite3_prepare_v2(this->db, command.c_str(), -1, &this->res, 0);
+        
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(this->db));
+            return;
+        }
+        
+        this->rc = sqlite3_step(this->res);
+        
+//        if (this->rc == SQLITE_ROW) {
+//            printf("%s\n", sqlite3_column_text(this->res, 0));
+//            printf("%s\n", sqlite3_column_text(this->res, 1));
+//        }
+        sqlite3_finalize(this->res);
+    }
+
+    std::pair<std::string, std::string> select(std::string table, std::string name)
+    {
+        std::pair<std::string, std::string> p;
+
+        std::string command = "select * from " + table + " where name = \"" + name + "\";";
+        
+        rc = sqlite3_prepare_v2(this->db, command.c_str(), -1, &this->res, 0);
+        
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "Failed to fetch data: %s\n", sqlite3_errmsg(this->db));
+            return p;
+        }
+        
+        this->rc = sqlite3_step(this->res);
+        
+        if (this->rc == SQLITE_ROW) {
+            p.first = (char*)sqlite3_column_text(this->res, 0);
+            p.second = (char*)sqlite3_column_text(this->res, 1);
+        }
+        sqlite3_finalize(this->res);
+        
+        return p;
+    }
 };
 
 
+mpz_class gmp_random(int n)
+{
+    mpz_class result;
+    mpz_t randNum;
+    gmp_randstate_t gmpRandState;
+    mpz_init(randNum);
+    gmp_randinit_default(gmpRandState);
+    gmp_randseed_ui(gmpRandState, time(NULL) );
+    mpz_urandomb(randNum, gmpRandState, n);
+
+    result = mpz_class(randNum);
+
+    return result;
+}
+
 std::string gen_key(mpz_class n, mpz_class g, mpz_class m, mpz_class& m1)
 {
-    int y = 3;
-    mpz_class r;
+    mpz_class r1;
+    mpz_class r2;
+
+    mpz_class y = gmp_random(512);
     
-    mpz_pow_ui(r.get_mpz_t(), m.get_mpz_t(), y);
+    mpz_powm(r1.get_mpz_t(), g.get_mpz_t(), y.get_mpz_t(), n.get_mpz_t());
+    mpz_powm(r2.get_mpz_t(), m.get_mpz_t(), y.get_mpz_t(), n.get_mpz_t());
     
+    m1 = r1;
     
-    return "";
+    return r2.get_str();
 }
 
 void handshake(database& d, std::string name, std::string n, std::string g, std::string m)
@@ -78,20 +143,63 @@ void handshake(database& d, std::string name, std::string n, std::string g, std:
     mpz_set_str(n_num.get_mpz_t(), n.c_str(), 10);
     mpz_set_str(n_num.get_mpz_t(), n.c_str(), 10);
 
-    
     std::string key = gen_key(n_num, g_num, m_num, m1_num);
     
     d.insert("unverified", name, key);
 }
 
+int authorise(database& d, std::string name, std::string c_key, std::string u_key, std::string sig)
+{
 
-
-
-
-
+    return 1;
+}
 
 int main(int argc, const char * argv[])
 {
+//    int s = atoi( argv[1] );
+//    std::string name = argv[2];
+//    std::string n = argv[3];
+//    std::string g = argv[4];
+//    std::string m = argv[5];
+//    std::string c_key = argv[6];
+//    std::string u_key = argv[7];
+//    std::string sig = argv[8];
+//    std::string t = argv[9];
+//
+//
+//    database d = database("test.db");
+//
+//    
+//    switch (s) {
+//        case 0:
+//            handshake(d, name, n, g, m);
+//            break;
+//        case 1:
+//            authorise(d, name, c_key, u_key, sig);
+//            break;
+//
+//        default:
+//            break;
+//    }
+    
+    
+    
+    mpz_class n;
+    mpz_nextprime( n.get_mpz_t(), gmp_random(256).get_mpz_t() );
+
+    mpz_class g = gmp_random(256);
+    mpz_class x = gmp_random(256);
+    mpz_class m;
+    mpz_class m1;
+
+    mpz_powm(m.get_mpz_t(), g.get_mpz_t(), x.get_mpz_t(), n.get_mpz_t());
+
+    mpz_class m2;
+    
+    std::cout << gen_key(n, gmp_random(256), m, m1) << std::endl;
+    
+    
+    
 //    int s = atoi( argv[1] );
 //    
 //    switch (s) {
@@ -113,10 +221,21 @@ int main(int argc, const char * argv[])
 //    mpz_powm(c.get_mpz_t(), a.get_mpz_t(), b.get_mpz_t(), n.get_mpz_t());
 //    
 //    std::cout << c.get_str() << std::endl;
-    
-    database d = database("test.db");
-    
-    d.insert("verified", "conor", "12312312312312");
+//    
+//    database d = database("test.db");
+//    
+//    d.insert("verified", "conor", "12312312312312");
+//    auto p = d.select("verified", "conor");
+//    
+//    std::cout << p.first << std::endl;
+//    std::cout << p.second << std::endl;
+//    
+//    d.remove("verified", "conor");
+//
+//    p = d.select("verified", "conor");
+//    
+//    std::cout << p.first << std::endl;
+//    std::cout << p.second << std::endl;
     
 //    int rc = sqlite3_open("test.db", &d.db);
 //    
@@ -144,8 +263,6 @@ int main(int argc, const char * argv[])
 //    
 //    sqlite3_finalize(d.res);
 //    sqlite3_close(d.db);
-    
-    printf("Goodbyte");
     
     return 0;
 }
